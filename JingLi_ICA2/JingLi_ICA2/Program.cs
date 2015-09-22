9e9919d40583df5bbfb22237bd4036fa1a0bceb3
jessieli93 <jli83@studentmail.nait.ca>
2015-09-22 15:46:26 -0600

    ICA2
    Just fixed the code

--- JingLi_ICA2/JingLi_ICA2/Program.cs
+++ JingLi_ICA2/JingLi_ICA2/Program.cs
@@ -10,6 +10,31 @@ namespace JingLi_ICA2
     {
         static void Main(string[] args)
         {
+            int number = 5;
+            args = new string[2];
+            args[0] = number.ToString();
+            string x = Console.ReadLine();
+            args[0] = (x[0].ToString());
+            x = x.Substring(1, x.Length - 2);
+            //Console.WriteLine(x);
+            args[1] = x;
+            //Console.WriteLine(args[0]);
+            //Console.WriteLine(args[1]);
+            //Console.ReadKey();
+            //args = x.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                for (int i = 0; i < Convert.ToInt64(args[0]); i++)
+
+
+                    Console.WriteLine(args[1]);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Print error");
+            }
+            Console.ReadKey();
         }
     }
 }
